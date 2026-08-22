5d227447a263aa3177efa73b1b359c9389428e1c
marwan nofal <marwannofal64@gmail.com>
2026-08-22 23:51:44 +0300

    Fix GitHub Pages Next.js deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-};
+  
+  trailingSlash: true,
 
-export default nextConfig;
+  images: {
+    unoptimized: true,
+  },
+};
